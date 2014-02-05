a3dd2ec29d34746aa8288b09fb290bf8623813ff
StefanSinapov <stefan.sinapov@gmail.com>
2014-02-05 12:53:46 +0200

    add homework 2 OOP

---
+++ 3. Обектно-ориентирано програмиране (ООП)/Homework/2. Defining Classes Part II/5.Generics/GenericList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+	public class GenericList<T> where T:IComparable
+	{
+		//Constant Fields
+		private const int DefaultCapacity = 1;
+
+		//Fields
+		private T[] elements;
+
+		//Constructors
+		public GenericList(int capacity=DefaultCapacity)
+		{
+			this.Count = 0;
+			this.Capacity = capacity;
+
+			this.elements = new T[this.Capacity];
+		}
+
+		//Properies
+		public int Count { get; private set; }
+		public int Capacity { get; private set; }
+
+		public T this[int index]
+		{
+			get 
+			{
+				if (index < 0 || index >= this.Count)
+					throw new ArgumentOutOfRangeException("Index is out of range");
+				return this.elements[index];
+			}
+			set { this.elements[index] = value; }
+		}
+
+		//Methods
+		public void Add(T element)
+		{
+			this.Count++;
+			this.Resize();
+			this.elements[this.Count-1] = element;
+		}
+		public void RemoveAt(int index)
+		{
+			if (index < 0 || index >= this.Count)
+				throw new ArgumentOutOfRangeException("Index is out of range");
+			this.Count--;
+			this.Resize();
+
+			Array.Copy(this.elements, index + 1, this.elements, index, this.Count - index);
+
+			//this.elements[this.Count] = default(T); ??
+		}
+
+		//Private Method
+		private void Resize()
+		{
+			if(this.Count>this.Capacity)
+			{
+				this.Capacity *= 2;
+				Array.Resize<T>(ref this.elements ,this.Capacity);
+			}
+			//todo smaller itself
+		}
+	}
+}
---
+++ 3. Обектно-ориентирано програмиране (ООП)/Homework/2. Defining Classes Part II/5.Generics/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * 5.Write a generic class GenericList that keeps a list of elements of some parametric type T.
+ * Keep the elements of the list in an array with fixed capacity which is given as parameter 
+ * in the class constructor. Implement methods for adding element, accessing element by index, removing 
+ * element by index, inserting element at given position, clearing the list, finding element by its value and ToString(). Check all input parameters to avoid accessing elements at invalid positions.
+ * 
+ * 6.Implement auto-grow functionality: when the internal array is full, create a new array 
+ * of double size and move all elements to it.
+ * 
+ * 7.Create generic methods Min() and Max() for finding the minimal and maximal element in the GenericList.
+ * You may need to add a generic constraints for the type T.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+	class Program
+	{
+		static void Main()
+		{
+		}
+	}
+}
