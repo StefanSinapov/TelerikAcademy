3643872d4f4830bd9afb26d450f09ecd81d45175
StefanSinapov <stefan.sinapov@gmail.com>
2014-05-04 11:15:47 +0300

    JavaScript and КПК course lectures and hw

---
+++ 7. High-Quality Code/Homework/1. Code Formatting/README.md
@@ -0,0 +1,3 @@
+1. Format correctly the following source code (given in Code-Formatting-Homework.zip):
+	- C# code given in the file events.cs.
+	- JavaScript code given in the file code.js.
---
+++ 7. High-Quality Code/Homework/1. Code Formatting/code.js
@@ -0,0 +1,116 @@
+var off = 0,
+    txt = "",
+    pX = 0,
+    pY = 0,
+    addScroll = false,
+    b = navigator.appName,
+    theLayer;
+
+if ((navigator.userAgent.indexOf('MSIE 5') > 0) || (navigator.userAgent.indexOf('MSIE 6')) > 0) {
+    addScroll = true;
+}
+
+function PopTip() {
+    "use strict";
+    if (b === "Netscape") {
+        theLayer = eval('document.layers[\'ToolTip\']');
+        if ((pX + 120) > window.innerWidth) {
+            pX = window.innerWidth - 150;
+        }
+        theLayer.left = pX + 10;
+        theLayer.top = pY + 15;
+        theLayer.visibility = 'show';
+    } else {
+        theLayer = eval('document.all[\'ToolTip\']');
+        if (theLayer) {
+            pX = event.x - 5;
+            pY = event.y;
+            if (addScroll) {
+                pX = pX + document.body.scrollLeft;
+                pY = pY + document.body.scrollTop;
+            }
+            if ((pX + 120) > document.body.clientWidth) {
+                pX = pX - 150;
+            }
+            theLayer.style.pixelLeft = pX + 10;
+            theLayer.style.pixelTop = pY + 15;
+            theLayer.style.visibility = 'visible';
+        }
+    }
+}
+
+function mouseMove(evn) {
+    "use strict";
+    if (b === "Netscape") {
+        pX = evn.pageX - 5;
+        pY = evn.pageY;
+    } else {
+        pX = event.x - 5;
+        pY = event.y;
+    }
+    if (b === "Netscape") {
+        if (document.layers["ToolTip"].visibility === 'show') {
+            PopTip();
+        }
+    } else {
+        if (document.all['ToolTip'].style.visibility === 'visible') {
+            PopTip();
+        }
+    }
+}
+
+document.onmousemove = mouseMove;
+if (b === "Netscape") {
+    document.captureEvents(Event.MOUSEMOVE)
+};
+
+
+
+
+
+function HideTip() {
+    args = HideTip.arguments;
+    if (b === "Netscape") {
+        document.layers['ToolTip'].visibility = 'hide';
+    } else {
+        document.all['ToolTip'].style.visibility = 'hidden';
+    }
+}
+
+function HideMenu1() {
+    if (b === "Netscape") {
+        document.layers['menu1'].visibility = 'hide';
+    } else {
+        document.all['menu1'].style.visibility = 'hidden';
+    }
+}
+
+function ShowMenu1() {
+    if (b === "Netscape") {
+        theLayer = eval('document.layers[\'menu1\']');
+        theLayer.visibility = 'show';
+    } else {
+        theLayer = eval('document.all[\'menu1\']');
+        theLayer.style.visibility = 'visible';
+    }
+}
+
+function HideMenu2() {
+    "use strict";
+    if (b === "Netscape") {
+        document.layers['menu2'].visibility = 'hide';
+    } else {
+        document.all['menu2'].style.visibility = 'hidden';
+    }
+}
+
+function ShowMenu2() {
+    "use strict";
+    if (b === "Netscape") {
+        theLayer = eval('document.layers[\'menu2\']');
+        theLayer.visibility = 'show';
+    } else {
+        theLayer = eval('document.all[\'menu2\']');
+        theLayer.style.visibility = 'visible';
+    }
+}
