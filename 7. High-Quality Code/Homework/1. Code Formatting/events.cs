d3396da9aac6857a7d03725b99820c09017db389
StefanSinapov <stefan.sinapov@gmail.com>
2014-05-14 14:34:20 +0300

    JavaScript and OOP hw and readme's

--- 7. High-Quality Code/Homework/1. Code Formatting/events.cs
+++ 7. High-Quality Code/Homework/1. Code Formatting/events.cs
@@ -1,210 +1,300 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using Wintellect.PowerCollections;
 
-class Event : IComparable
+internal class Event : IComparable
 {
 	public DateTime date;
-	public String title;
-	public String location;
 
-	public Event(DateTime date, String title, String location)
+	public string title;
+
+	public string location;
+
+	public Event(DateTime date, string title, string location)
 	{
 		this.date = date;
-
 		this.title = title;
 		this.location = location;
 	}
 
 	public int CompareTo(object obj)
 	{
+		int num;
 		Event other = obj as Event;
 		int byDate = this.date.CompareTo(other.date);
 		int byTitle = this.title.CompareTo(other.title);
-
 		int byLocation = this.location.CompareTo(other.location);
-		if (byDate == 0)
+		bool flag = byDate != 0;
+		if (flag)
 		{
-			if (byTitle == 0)
-				return byLocation;
-			else
-			{ return byTitle; }
-
-
+			num = byDate;
 		}
 		else
-			return byDate;
+		{
+			flag = byTitle != 0;
+			num = (flag ? byTitle : byLocation);
+		}
+		return num;
 	}
 
 	public override string ToString()
 	{
+		bool flag;
 		StringBuilder toString = new StringBuilder();
-		toString.Append(date
-			.ToString("yyyy-MM-ddTHH:mm:ss"));
-
-
-		toString.Append(" | " + title);
-		if (location != null && location != "") toString.Append(" | " + location);
-		return toString.ToString();
+		toString.Append(this.date.ToString("yyyy-MM-ddTHH:mm:ss"));
+		toString.Append(string.Concat(" | ", this.title));
+		flag = (this.location == null ? true : !(this.location != ""));
+		bool flag1 = flag;
+		if (!flag1)
+		{
+			toString.Append(string.Concat(" | ", this.location));
+		}
+		string str = toString.ToString();
+		return str;
 	}
 }
 
-class Program
+internal class Program
 {
-	static StringBuilder output = new StringBuilder();
+	private static StringBuilder output;
 
-	static class Messages
+	private static Program.EventHolder events;
+
+	static Program()
 	{
-		public static void EventAdded()
-		{ output.Append("Event added\n"); }
-		public static void EventDeleted(int x)
-		{
-			if (x == 0) NoEventsFound();
-
-			else output.AppendFormat("{0} events deleted\n", x);
-		}
-		public static void NoEventsFound() { output.Append("No events found\n"); }
-		public static void PrintEvent(Event eventToPrint)
-		{
-
-
-			if (eventToPrint != null)
-			{
-				output.Append(eventToPrint + "\n");
-			}
-		}
+		Program.output = new StringBuilder();
+		Program.events = new Program.EventHolder();
 	}
 
-	class EventHolder
+	public Program()
 	{
-		MultiDictionary<string, Event>
-			byTitle = new MultiDictionary<string,
-			Event>(true);
-		OrderedBag<Event>
-			byDate = new OrderedBag<
-			Event>();
-
-		public void AddEvent(DateTime date,
-							 string title,
-							 string location)
-		{
-			Event newEvent = new Event(date,
-									   title,
-									   location);
-			byTitle.Add(
-						title.ToLower(),
-						newEvent
-						);
-			byDate.Add(newEvent); Messages.EventAdded();
-		}
-
-		public void DeleteEvents(string titleToDelete)
-		{
-			string title = titleToDelete
-					.ToLower();
-			int removed = 0;
-			foreach (var eventToRemove in byTitle[title])
-			{
-				removed++;
-				byDate.Remove(eventToRemove);
-			}
-			byTitle.Remove(title);
-			Messages.EventDeleted(removed);
-		}
-		public void ListEvents(DateTime date, int count)
-		{
-			OrderedBag<Event>.View
-					eventsToShow = byDate.RangeFrom(new Event(
-					date, "", ""),
-					true);
-			int showed = 0;
-			foreach (var eventToShow in eventsToShow)
-			{
-				if (showed == count) break;
-				Messages.PrintEvent(eventToShow);
-
-				showed++;
-			}
-			if (showed == 0) Messages.NoEventsFound();
-		}
-	} static EventHolder events = new EventHolder();
-
-	static void Main(string[] args)
-	{
-		while (ExecuteNextCommand()) { }
-		Console.WriteLine(output);
-	}
-
-	private static bool ExecuteNextCommand()
-	{
-		string command = Console.ReadLine();
-		if (command[0] == 'A') { AddEvent(command); return true; }
-		if (command[0] == 'D') { DeleteEvents(command); return true; }
-
-
-		if (command[0] == 'L') { ListEvents(command); return true; }
-		if (command[0] == 'E') { return false; }
-		return false;
-	}
-
-	private static void ListEvents(string command)
-	{
-		int pipeIndex = command.IndexOf('|')
-		;
-		DateTime date = GetDate(
-		command,
-		"ListEvents");
-		string countString = command.Substring(
-			pipeIndex + 1
-		);
-
-
-		int count = int.Parse(countString);
-		events.ListEvents(date,
-		count);
-	}
-
-	private static void DeleteEvents(string command)
-	{
-		string title = command.Substring
-		(
-			"DeleteEvents".Length + 1
-		);
-		events.DeleteEvents(title);
 	}
 
 	private static void AddEvent(string command)
 	{
-		DateTime date; string title; string location;
-		GetParameters(command, "AddEvent", out date, out title, out location);
-		events.AddEvent(date, title, location);
+		DateTime date;
+		string title;
+		string location;
+		Program.GetParameters(command, "AddEvent", out date, out title, out location);
+		Program.events.AddEvent(date, title, location);
 	}
-	private static void GetParameters(string commandForExecution, string commandType, out DateTime dateAndTime, out string eventTitle, out string eventLocation)
+
+	private static void DeleteEvents(string command)
 	{
-		dateAndTime = GetDate(commandForExecution, commandType);
-		int firstPipeIndex = commandForExecution.IndexOf('|');
+		string title = command.Substring("DeleteEvents".Length + 1);
+		Program.events.DeleteEvents(title);
+	}
 
-
-		int lastPipeIndex = commandForExecution.LastIndexOf('|');
-		if (firstPipeIndex == lastPipeIndex)
+	private static bool ExecuteNextCommand()
+	{
+		bool flag;
+		string command = Console.ReadLine();
+		bool flag1 = command[0] != 'A';
+		if (flag1)
 		{
-			eventTitle = commandForExecution.Substring(firstPipeIndex + 1).Trim();
-			eventLocation = string.Empty;
+			flag1 = command[0] != 'D';
+			if (flag1)
+			{
+				flag1 = command[0] != 'L';
+				if (flag1)
+				{
+					flag1 = command[0] != 'E';
+					flag = (flag1 ? false : false);
+				}
+				else
+				{
+					Program.ListEvents(command);
+					flag = true;
+				}
+			}
+			else
+			{
+				Program.DeleteEvents(command);
+				flag = true;
+			}
 		}
 		else
 		{
-			eventTitle = commandForExecution.Substring(
-													firstPipeIndex + 1,
-													lastPipeIndex - firstPipeIndex - 1)
-													.Trim();
-			eventLocation = commandForExecution.Substring(lastPipeIndex + 1).Trim();
+			Program.AddEvent(command);
+			flag = true;
 		}
+		return flag;
 	}
+
 	private static DateTime GetDate(string command, string commandType)
 	{
 		DateTime date = DateTime.Parse(command.Substring(commandType.Length + 1, 20));
-		return date;
+		DateTime dateTime = date;
+		return dateTime;
+	}
+
+	private static void GetParameters(string commandForExecution, string commandType, out DateTime dateAndTime, out string eventTitle, out string eventLocation)
+	{
+		dateAndTime = Program.GetDate(commandForExecution, commandType);
+		int firstPipeIndex = commandForExecution.IndexOf('|');
+		int lastPipeIndex = commandForExecution.LastIndexOf('|');
+		bool flag = firstPipeIndex != lastPipeIndex;
+		if (flag)
+		{
+			eventTitle = commandForExecution.Substring(firstPipeIndex + 1, lastPipeIndex - firstPipeIndex - 1).Trim();
+			eventLocation = commandForExecution.Substring(lastPipeIndex + 1).Trim();
+		}
+		else
+		{
+			eventTitle = commandForExecution.Substring(firstPipeIndex + 1).Trim();
+			eventLocation = "";
+		}
+	}
+
+	private static void ListEvents(string command)
+	{
+		int pipeIndex = command.IndexOf('|');
+		DateTime date = Program.GetDate(command, "ListEvents");
+		string countString = command.Substring(pipeIndex + 1);
+		int count = int.Parse(countString);
+		Program.events.ListEvents(date, count);
+	}
+
+	private static void Main(string[] args)
+	{
+		while (true)
+		{
+			bool flag = Program.ExecuteNextCommand();
+			if (!flag)
+			{
+				break;
+			}
+		}
+		Console.WriteLine(Program.output);
+	}
+
+	private class EventHolder
+	{
+		private MultiDictionary<string, Event> byTitle;
+
+		private OrderedBag<Event> byDate;
+
+		public EventHolder()
+		{
+		}
+
+		public void AddEvent(DateTime date, string title, string location)
+		{
+			Event newEvent = new Event(date, title, location);
+			this.byTitle.Add(title.ToLower(), newEvent);
+			this.byDate.Add(newEvent);
+			Program.Messages.EventAdded();
+		}
+
+		public void DeleteEvents(string titleToDelete)
+		{
+			bool flag;
+			string title = titleToDelete.ToLower();
+			int removed = 0;
+			IEnumerator<Event> enumerator = this.byTitle[title].GetEnumerator();
+			try
+			{
+				while (true)
+				{
+					flag = enumerator.MoveNext();
+					if (!flag)
+					{
+						break;
+					}
+					Event eventToRemove = enumerator.Current;
+					removed++;
+					this.byDate.Remove(eventToRemove);
+				}
+			}
+			finally
+			{
+				flag = enumerator == null;
+				if (!flag)
+				{
+					enumerator.Dispose();
+				}
+			}
+			this.byTitle.Remove(title);
+			Program.Messages.EventDeleted(removed);
+		}
+
+		public void ListEvents(DateTime date, int count)
+		{
+			bool flag;
+			OrderedBag<Event>.View eventsToShow = this.byDate.RangeFrom(new Event(date, "", ""), true);
+			int showed = 0;
+			IEnumerator<Event> enumerator = eventsToShow.GetEnumerator();
+			try
+			{
+				while (true)
+				{
+					flag = enumerator.MoveNext();
+					if (!flag)
+					{
+						break;
+					}
+					Event eventToShow = enumerator.Current;
+					flag = showed != count;
+					if (flag)
+					{
+						Program.Messages.PrintEvent(eventToShow);
+						showed++;
+					}
+					else
+					{
+						break;
+					}
+				}
+			}
+			finally
+			{
+				flag = enumerator == null;
+				if (!flag)
+				{
+					enumerator.Dispose();
+				}
+			}
+			flag = showed != 0;
+			if (!flag)
+			{
+				Program.Messages.NoEventsFound();
+			}
+		}
+	}
+
+	private static class Messages
+	{
+		public static void EventAdded()
+		{
+			Program.output.Append("Event added\n");
+		}
+
+		public static void EventDeleted(int x)
+		{
+			bool flag = x != 0;
+			if (flag)
+			{
+				Program.output.AppendFormat("{0} events deleted\n", x);
+			}
+			else
+			{
+				Program.Messages.NoEventsFound();
+			}
+		}
+
+		public static void NoEventsFound()
+		{
+			Program.output.Append("No events found\n");
+		}
+
+		public static void PrintEvent(Event eventToPrint)
+		{
+			bool flag = eventToPrint == null;
+			if (!flag)
+			{
+				Program.output.Append(string.Concat(eventToPrint, "\n"));
+			}
+		}
 	}
 }
