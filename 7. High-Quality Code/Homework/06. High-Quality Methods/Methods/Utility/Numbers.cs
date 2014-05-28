24009f1e15c7e32807aa87adf05099767edf5642
StefanSinapov <stefan.sinapov@gmail.com>
2014-05-28 11:53:27 +0300

    High-Quality Code all lectures + hw 6

---
+++ 7. High-Quality Code/Homework/06. High-Quality Methods/Methods/Utility/Numbers.cs
@@ -0,0 +1,52 @@
+﻿namespace Methods.Utility
+{
+    using System;
+
+    public static class Numbers
+    {
+        public static string NumberToDigit(int digit)
+        {
+            if (digit < 0 || digit > 9)
+            {
+                throw new ArgumentOutOfRangeException("Number must be in range [0 - 9]");
+            }
+            string result = string.Empty;
+            switch (digit)
+            {
+                case 0:
+                    result = "zero";
+                    break;
+                case 1:
+                    result = "one";
+                    break;
+                case 2:
+                    result = "two";
+                    break;
+                case 3:
+                    result = "three";
+                    break;
+                case 4:
+                    result = "four";
+                    break;
+                case 5:
+                    result = "five";
+                    break;
+                case 6:
+                    result = "six";
+                    break;
+                case 7:
+                    result = "seven";
+                    break;
+                case 8:
+                    result = "eight";
+                    break;
+                case 9:
+                    result = "nine";
+                    break;
+            }
+            return result;
+        }
+
+
+    }
+}
