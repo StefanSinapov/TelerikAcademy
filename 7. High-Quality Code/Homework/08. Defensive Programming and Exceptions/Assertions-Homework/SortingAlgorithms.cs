662dc646f02755e1b2a0941bbee37c14ccf46e4f
StefanSinapov <stefan.sinapov@gmail.com>
2014-06-04 14:25:46 +0300

    High-Quality Code Homework 08 - task 1

---
+++ 7. High-Quality Code/Homework/08. Defensive Programming and Exceptions/Assertions-Homework/SortingAlgorithms.cs
@@ -0,0 +1,45 @@
+﻿namespace Assertions_Homework
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+
+    public static class SortingAlgorithms
+    {
+        public static void SelectionSort<T>(T[] arr) where T : IComparable<T>
+        {
+            Debug.Assert(arr != null, "Array is null");
+            Debug.Assert(arr.Length > 0, "Array is empty");
+            Debug.Assert(arr.Length != 1, "Array contains single element");
+
+            for (int index = 0; index < arr.Length - 1; index++)
+            {
+                int minElementIndex = FindMinElementIndex(arr, index, arr.Length - 1);
+                Swap(ref arr[index], ref arr[minElementIndex]);
+            }
+        }
+
+        private static int FindMinElementIndex<T>(T[] arr, int startIndex, int endIndex)
+            where T : IComparable<T>
+        {
+            int minElementIndex = startIndex;
+            for (int i = startIndex + 1; i <= endIndex; i++)
+            {
+                if (arr[i].CompareTo(arr[minElementIndex]) < 0)
+                {
+                    minElementIndex = i;
+                }
+            }
+
+            return minElementIndex;
+        }
+
+        private static void Swap<T>(ref T x, ref T y)
+        {
+            T oldX = x;
+            x = y;
+            y = oldX;
+        }
+    }
+}
---
+++ 7. High-Quality Code/Homework/08. Defensive Programming and Exceptions/README.md
@@ -0,0 +1,4 @@
+## 08. Defensive Programming and Exceptions
+
+1. Add assertions in the code from the project `Assertions-Homework` to ensure all possible preconditions and postconditions are checked.
+2. Add exception handling (where missing) and refactor all incorrect error handling in the code from the `Exceptions-Homework` project to follow the best practices for using exceptions.
