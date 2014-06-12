db492d6015e8f7eeefa160d94e8020b153af07d8
StefanSinapov <stefan.sinapov@gmail.com>
2014-06-12 22:55:06 +0300

    DOM & UI HW 05 Raphael - 03. Spiral - changes

--- 8. JavaScript UI & DOM/Homework/05. Raphael JS for SVG/03. Spiral/scripts/script.js
+++ 8. JavaScript UI & DOM/Homework/05. Raphael JS for SVG/03. Spiral/scripts/script.js
@@ -2,13 +2,8 @@
  * Created by Stefan on 11.6.2014 г..
  */
 
-window.onload = function (){
-    var paper = Raphael(0, 0, 600, 600);
-    drawBorder(paper);
-    drawSpiral(paper);
-};
-
-function drawBorder(paper){
+function drawBorder(paper) {
+    'use strict';
     paper.rect(3, 3, 500, 490, 15).attr({
         fill: 'none',
         stroke: '#DAEDF2',
@@ -16,28 +11,38 @@ function drawBorder(paper){
     });
 }
 
-function drawSpiral(paper) {
-
-    var space = 8,
-        scale = 10,
-        centerX = 250,
-        centerY = 250,
-        STEPS_PER_ROTATION = space * 100,
-        increment = 2 * Math.PI / STEPS_PER_ROTATION,
-        angle = increment;
-
-    while (angle < scale * Math.PI) {
-        var newX = centerX + (space * angle) * Math.cos(angle);
-        var newY = centerY - (space * angle) * Math.sin(angle);
-        drawCircle(paper, newX, newY);
-        angle += increment;
-    }
-}
-
 function drawCircle(paper, cx, cy) {
+    'use strict';
     paper.circle(cx, cy, 1)
         .attr({
             stroke: 'black',
             fill: 'black'
         });
 }
+
+function drawSpiral(paper) {
+    'use strict';
+    var space = 8,
+        scale = 10,
+        centerX = 250,
+        centerY = 250,
+        STEPS_PER_ROTATION = space * 100,
+        increment = 2 * Math.PI / STEPS_PER_ROTATION,
+        angle = increment,
+        newX,
+        newY;
+    while (angle < scale * Math.PI) {
+        newX = centerX + (space * angle) * Math.cos(angle);
+        newY = centerY - (space * angle) * Math.sin(angle);
+        drawCircle(paper, newX, newY);
+        angle += increment;
+    }
+}
+
+window.onload = function () {
+    'use strict';
+    var paper = Raphael(0, 0, 600, 600);
+    drawBorder(paper);
+    drawSpiral(paper);
+};
+
