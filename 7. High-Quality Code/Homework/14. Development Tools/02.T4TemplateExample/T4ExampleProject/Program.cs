de4660aad1303615fcac25c7a02c60baa47893ef
StefanSinapov <stefan.sinapov@gmail.com>
2014-06-29 17:50:25 +0300

    HQQ homework 14. Development Tools - complete

---
+++ 7. High-Quality Code/Homework/14. Development Tools/02.T4TemplateExample/T4ExampleProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4ExampleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
