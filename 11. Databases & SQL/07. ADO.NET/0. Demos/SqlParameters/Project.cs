a617d6c4d9dbc890bf1b7f7bfee7dd1bbba14ec8
StefanSinapov <stefan.sinapov@gmail.com>
2014-08-26 18:52:03 +0300

    DB - 07. ADO.NET - Lecture and Demos

---
+++ 11. Databases & SQL/07. ADO.NET/0. Demos/SqlParameters/Project.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Project
+{
+    public Project(int id, string name, string description, DateTime startDate, DateTime? endDate)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.Description = description;
+        this.StartDate = startDate;
+        this.EndDate = endDate;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+
+    public override string ToString()
+    {
+        string text = String.Format(
+            "Project(Id = {0}, \n\tName = {1}, \n\tDescription = {2}, \n\tStartDate = {3}, \n\tEndDate = {4})",
+            this.Id, this.Name, this.Description, this.StartDate, this.EndDate);
+        return text;
+    }
+}
---
+++ 11. Databases & SQL/07. ADO.NET/0. Demos/SqlParameters/SqlParametersExample.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Data.SqlClient;
+using SqlParametersDemo;
+using System.Configuration;
+
+class SqlParametersExample
+{
+    private SqlConnection dbCon;
+
+    private void ConnectToDB()
+    {
+        // see app.config if chang to ConnectionString needed
+        ConnectionStringSettings dBConnectionString = ConfigurationManager.ConnectionStrings["SQLDB"];
+        dbCon = new SqlConnection(dBConnectionString.ConnectionString);//Settings.Default.DBConnectionString);
+        dbCon.Open();
+    }
+
+    private void DisconnectFromDB()
+    {
+        if (dbCon != null)
+        {
+            dbCon.Close();
+        }
+    }
+
+    private Project GetProjectById(int id)
+    {
+        SqlCommand cmdSelectProject = new SqlCommand(
+            "SELECT * FROM Projects WHERE ProjectID = @id", this.dbCon);
+        cmdSelectProject.Parameters.AddWithValue("@id", id);
+        SqlDataReader reader = cmdSelectProject.ExecuteReader();
+        using (reader)
+        {
+            if (reader.Read())
+            {
+                string name = (string)reader["Name"];
+                string description = (string)reader["Description"];
+                DateTime startDate = (DateTime)reader["StartDate"];
+                object endDateObj = reader["EndDate"];
+                DateTime? endDate = null;
+                if (endDateObj != DBNull.Value)
+                {
+                    endDate = (DateTime)endDateObj;
+                }
+
+                Project project = new Project(id, name, description, startDate, endDate);
+                return project;
+            }
+            else
+            {
+                throw new ArgumentException(
+                    String.Format("Invalid project (ID = {0}).", id));
+            }
+        }
+    }
+
+    private int InsertProject(string name, string description,
+        DateTime startDate, DateTime? endDate)
+    {
+        SqlCommand cmdInsertProject = new SqlCommand(
+            "INSERT INTO Projects(Name, Description, StartDate, EndDate) " +
+            "VALUES (@name, @description, @startDate, @endDate)", dbCon);
+        cmdInsertProject.Parameters.AddWithValue("@name", name);
+        cmdInsertProject.Parameters.AddWithValue("@description", description);
+        cmdInsertProject.Parameters.AddWithValue("@startDate", startDate);
+        SqlParameter sqlParameterEndDate = new SqlParameter("@endDate", endDate);
+        if (endDate == null)
+        {
+            sqlParameterEndDate.Value = DBNull.Value;
+        }
+        cmdInsertProject.Parameters.Add(sqlParameterEndDate);
+        cmdInsertProject.ExecuteNonQuery();
+
+        SqlCommand cmdSelectIdentity = new SqlCommand("SELECT @@Identity", dbCon);
+        int insertedRecordId = (int)(decimal)cmdSelectIdentity.ExecuteScalar();
+        return insertedRecordId;
+    }
+
+    static void Main()
+    {
+        SqlParametersExample example = new SqlParametersExample();
+        try
+        {
+            example.ConnectToDB();
+
+            // Retrieve a project from the "Projects" table
+            Project project = example.GetProjectById(10);
+            Console.WriteLine(project);
+
+            // Insert new project in the "Projects" table
+            int newProjectId = example.InsertProject("New project",
+                "Some description", DateTime.Now, null);
+            Console.WriteLine("Inserted new project. " +
+                "ProjectID = {0}", newProjectId);
+        }
+        finally
+        {
+            example.DisconnectFromDB();
+        }
+    }
+}
