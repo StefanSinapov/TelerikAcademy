9bcbbcd164b4aed8075b887d06787f6c28830d5d
StefanSinapov <stefan.sinapov@gmail.com>
2014-08-27 11:01:51 +0300

    Databases - 07. ADO.NET - first 5 tasks

---
+++ 11. Databases & SQL/07. ADO.NET/01. Retrieve Number Of Records/NumberOfRecords.cs
@@ -0,0 +1,23 @@
+﻿namespace _01.Retrieve_Number_Of_Records
+{
+    using System;
+    using System.Configuration;
+    using System.Data.SqlClient;
+
+    public class NumberOfRecords
+    {
+        // NOTE: please edit connection string in App.config to fit your server address.
+        public static void Main()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Northwind"].ConnectionString;
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            using (sqlConnection)
+            {
+                SqlCommand sqlCommand = new SqlCommand("SELECT COUNT(CategoryId) FROM Categories", sqlConnection);
+                int countOfCategories = (int)sqlCommand.ExecuteScalar();
+                Console.WriteLine("Count of all Categories is: {0}", countOfCategories);
+            }
+        }
+    }
+}
