082f7e61a2e4d93a89cd6bd60dbdb3b5bfe11907
StefanSinapov <stefan.sinapov@gmail.com>
2014-08-30 05:40:54 +0300

    DB - 08. Entity Framework - task 1, 2

---
+++ 11. Databases & SQL/08. ORM and Entity Framework/01. Creating Northwind Entities/NorthWindEntitiesEntryPoint.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * 1. Using the Visual Studio Entity Framework 
+ * designer create a `DbContext` for the `Northwind` database
+ */
+namespace _01.Creating_Northwind_Entities
+{
+    using System;
+    using System.Linq;
+    using Northwind.Model;
+
+    class NorthWindEntitiesEntryPoint
+    {
+        static void Main()
+        {
+            var northwindDb = new NorthwindEntities();
+
+            using (northwindDb)
+            {
+                var categories = northwindDb.Categories.Select(category =>
+                    new
+                    {
+                        Name = category.CategoryName,
+                        category.Description
+                    });
+
+                foreach (var category in categories)
+                {
+                    Console.WriteLine("Category {0} - {1}", category.Name, category.Description);
+                }
+            }
+        }
+    }
+}
---
+++ 11. Databases & SQL/08. ORM and Entity Framework/02. Data Access Objects/DataAccessDemo.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * 2. Create a DAO class with static methods which provide 
+ * functionality for inserting, modifying and deleting customers. 
+ * Write a testing class.
+ */
+namespace _02.Data_Access_Objects
+{
+    using System;
+    using Northwind.Model;
+
+    class DataAccessDemo
+    {
+        static void Main()
+        {
+            var db = new NorthwindDao();
+
+            var customer = new Customer
+            {
+                CustomerID = "Test1",
+                CompanyName = "Our Company Inc.",
+                ContactName = "Pesho",
+                ContactTitle = "Test Title"
+            };
+
+            Console.WriteLine("{0} customer(s) added.", db.InsertCustomer(customer));
+
+            Console.WriteLine("{0} customer(s) updated.", db.ModifyCustomer("Test1", "Some other Company"));
+
+            Console.WriteLine("{0} customer(s) deleted.", db.DeleteCustomerById("Test1"));
+        }
+    }
+}
