4f85f2555218c4ee8b1d2735c5e466ba6f37e1df
StefanSinapov <stefan.sinapov@gmail.com>
2014-09-14 03:57:57 +0300

    DSA - Exam Preparation

---
+++ 12. Data Structures & Algorithms/13. Exam Preparation/2013/Sample-Exam/01. Messages in Bottle/MessagesInBottleDemo.cs
@@ -0,0 +1,82 @@
+﻿namespace _01.Messages_in_Bottle
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class MessagesInBottleDemo
+    {
+        private static readonly Dictionary<char, string> Cipher = new Dictionary<char, string>();
+        private static readonly SortedSet<string> ResultSet = new SortedSet<string>();
+        private static string secredMessage;
+
+        static void Main()
+        {
+#if DEBUG
+            Console.SetIn(new System.IO.StreamReader("../../input.txt"));
+#endif
+
+            secredMessage = Console.ReadLine();
+            var cipherString = Console.ReadLine();
+
+            char key = char.MinValue;
+            var value = new StringBuilder();
+            for (int i = 0; i < cipherString.Length; i++)
+            {
+                if ('A' <= cipherString[i] && cipherString[i] <= 'Z')
+                {
+
+                    if (i > 0)
+                    {
+                        if (Cipher.ContainsKey(key))
+                        {
+                            Cipher[key] = value.ToString();
+                        }
+                        else
+                        {
+                            Cipher.Add(key, value.ToString());
+                        }
+                        value.Clear();
+                    }
+
+                    key = cipherString[i];
+                }
+                else
+                {
+                    value.Append(cipherString[i]);
+                }
+            }
+
+            Cipher.Add(key, value.ToString());
+            value.Clear();
+
+            Decipher(0, new StringBuilder());
+
+            Console.WriteLine(ResultSet.Count);
+            foreach (var result in ResultSet)
+            {
+                Console.WriteLine(result);
+            }
+        }
+
+        private static void Decipher(int secredMsgIndex, StringBuilder result)
+        {
+            if (secredMsgIndex == secredMessage.Length)
+            {
+                ResultSet.Add(result.ToString());
+                return;
+            }
+
+            foreach (var item in Cipher)
+            {
+                if (secredMessage.Substring(secredMsgIndex).StartsWith(item.Value))
+                {
+                    Decipher(secredMsgIndex + item.Value.Length, result.Append(item.Key));
+                    result.Length--;
+                }
+            }
+        }
+    }
+
+
+}
