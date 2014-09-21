aabe131749c8526199f500aa64ac46e511bdd4ad
StefanSinapov <stefan.sinapov@gmail.com>
2014-09-21 13:33:10 +0300

    Web Services and Clound - 03. WCF - complete

---
+++ 13. Web Services and Cloud/03. Windows Communication Foundation (WCF)/02. Substring.Console/SubstringConsoleDemo.cs
@@ -0,0 +1,57 @@
+﻿namespace Substring.Console
+{
+    using System;
+    using System.ServiceModel;
+    using System.ServiceModel.Description;
+    using Services;
+    using ServiceSubstring;
+
+    class SubstringConsoleDemo
+    {
+        private const string HostUrl = "http://localhost:8733/Design_Time_Addresses/Substring.Services/";
+
+        static void Main()
+        {
+            try
+            {
+                // Try to open host -> if host is already open it's throws an exception
+                using (var selfHost = HostSubstringModuleService())
+                {
+                    selfHost.Open();
+                    GetNumberOfSubstrings();
+                }
+            }
+            catch (Exception)
+            {
+                // Use already opened host
+                GetNumberOfSubstrings();
+            }
+        }
+
+        private static ServiceHost HostSubstringModuleService()
+        {
+            var serviceAddress = new Uri(HostUrl);
+            var smb = new ServiceMetadataBehavior
+            {
+                HttpGetEnabled = true
+            };
+
+            var selfHost = new ServiceHost(typeof(SubstringService), serviceAddress);
+            selfHost.Description.Behaviors.Add(smb);
+            return selfHost;
+        }
+
+        private static void GetNumberOfSubstrings()
+        {
+            var client = new SubstringServiceClient();
+
+            Console.Write("Text: ");
+            var text = Console.ReadLine();
+
+            Console.Write("Substring: ");
+            var substring = Console.ReadLine();
+
+            Console.WriteLine("Number of substrings: " + client.GetNumberOfSubstrings(text, substring));
+        }
+    }
+}
---
+++ 13. Web Services and Cloud/03. Windows Communication Foundation (WCF)/02. Substring.Services/ISubstringService.cs
@@ -0,0 +1,11 @@
+﻿namespace Substring.Services
+{
+    using System.ServiceModel;
+
+    [ServiceContract]
+    public interface ISubstringService
+    {
+        [OperationContract]
+        int GetNumberOfSubstrings(string text, string substring);
+    }
+}
