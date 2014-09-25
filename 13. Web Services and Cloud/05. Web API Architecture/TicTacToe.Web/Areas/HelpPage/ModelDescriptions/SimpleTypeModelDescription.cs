30a3350632c202d1cfb1590516440311fd1c8afc
StefanSinapov <stefan.sinapov@gmail.com>
2014-09-25 19:07:15 +0300

    Web services architecture homework

--- 13. Web Services and Cloud/05. Web API Architecture/TicTacToe.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace TicTacToe.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- 13. Web Services and Cloud/05. Web API Architecture/TicTacToe.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TicTacToe.Web.WebApiApplication" Language="C#" %>
