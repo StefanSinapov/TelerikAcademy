2c316aec68b4654f8caad5f69e8a053d0a4ce63c
StefanSinapov <stefan.sinapov@gmail.com>
2014-10-28 00:25:38 +0200

    ASP.NET - User Controls - homework

---
+++ 16. ASP.NET Web Forms/13. User Controls/01. Menu User Control/Models/Link.cs
@@ -0,0 +1,11 @@
+﻿namespace MenuUserControl.Models
+{
+    public class Link
+    {
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+
+        public string FontColor { get; set; }
+    }
+}
